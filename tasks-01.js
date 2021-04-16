174e335ced2099b4801fa118ffd031324eaa5a7f
AndriiDe <80851273+AndriiDe@users.noreply.github.com>
2021-04-16 16:21:16 +0300

    Create tasks-01.js

---
+++ tasks-01.js
@@ -0,0 +1,16 @@
+///https://www.codewars.com/kata/5772da22b89313a4d50012f7/javascript
+
+function greet (name, owner) {
+  if (name==owner) return "Hello boss";
+  else (name!==owner)
+  return "Hello guest";
+  }
+ 
+ ///https://www.codewars.com/kata/563a631f7cbbc236cf0000c2
+ 
+ function move (position, roll) {
+ const otherPosition = position+roll*2;
+ return otherPosition;
+}
+ 
+ 
